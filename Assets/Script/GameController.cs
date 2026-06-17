a9b58b1e364e0432c61193362483203bb73504cf
Everson Alijaya <alijayaeverson@gmail.com>
2026-06-17 10:47:13 -0400

    Fix brace placement in GameController methods

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
@@ -10,21 +12,40 @@ public class GameController : MonoBehaviour
     public float startWait;
     public float waveWait;
 
+    public TMP_Text scoreText;
+    public TMP_Text restartText;
+    public TMP_Text gameOverText;
+
     private bool gameOver;
+    private bool restart;
     private int score;
 
     private void Start()
     {
         gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
         score = 0;
-        Debug.Log("Score: " + score);
+        UpdateScore();
         StartCoroutine(SpawnWaves());
+    }
+
+    private void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
     }
 
     private IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
-        while (!gameOver)
+        while (true)
         {
             for (int i = 0; i < hazardCount; i++)
             {
@@ -40,18 +61,30 @@ public class GameController : MonoBehaviour
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
         }
     }
 
     public void AddScore(int newScoreValue)
     {
         score += newScoreValue;
-        Debug.Log("Score: " + score);
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
     }
 
     public void GameOver()
     {
+        gameOverText.text = "Game Over!";
         gameOver = true;
-        Debug.Log("Game Over");
     }
 }
