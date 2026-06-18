92032e186023ab7c4e693eb96c3f3981994d6654
Everson Alijaya <alijayaeverson@gmail.com>
2026-06-18 02:29:59 -0400

    Change fire button to space bar

--- Assets/Script/playercontroller.cs
+++ Assets/Script/playercontroller.cs
@@ -25,7 +25,7 @@ public class playercontroller : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        if Input.GetKey(KeyCode.Space) && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
